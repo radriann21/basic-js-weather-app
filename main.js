0ca97c9f95e8544f7c2b9d5e191fc56749be74ca
radriann21 <adrianrangelargu@gmail.com>
2024-09-12 00:01:57 -0400

    finished basic project

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
 import { getUserWeather } from "./services/getUserWeather.js";
+import { getInputWeather } from "./services/getInputWeather.js";
 
 const weatherImg = document.getElementById('weatherImg')
 const weather = document.getElementById('weather')
 const description = document.getElementById('description')
+const cityForm = document.getElementById('cityForm')
+const cityInput = document.getElementById('cityInput')
+
+let city = ''
 
 function updateUI(data) {
   weatherImg.src = 'https://openweathermap.org/img/w/' + data.weather[0].icon + '.png'
@@ -10,13 +15,45 @@ function updateUI(data) {
   description.innerText = data.weather[0].description
 }
 
+cityInput.addEventListener('change', (evt) => {
+  city = evt.target.value
+})
+
+cityForm.addEventListener('submit', async (evt) => {
+  evt.preventDefault()
+  try {
+    const data = await getInputWeather(city)
+    updateUI(data)
+  } catch(err) {
+    weather.innerText = 'Weather data not avaliable'
+    description.innerText = ''
+    weatherImg.src = ''
+  }
+})
+
 function setUserWeather() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(async (position) => {
-      const { latitude, longitude } = position.coords
-      const data = await getUserWeather(latitude, longitude)
-      updateUI(data)
-    })
+      const { latitude, longitude } = position.coords;
+      try {
+        const data = await getUserWeather(latitude, longitude);
+        updateUI(data);
+      } catch (error) {
+        weather.innerText = 'Error fetching weather data';
+        description.innerText = '';
+        weatherImg.src = '';
+        console.error('Error fetching weather data:', error);
+      }
+    }, (error) => {
+      weather.innerText = 'Geolocation error: ' + error.message;
+      description.innerText = '';
+      weatherImg.src = '';
+      console.error('Geolocation error:', error);
+    });
+  } else {
+    weather.innerText = 'Your browser does not support geolocation!';
+    description.innerText = '';
+    weatherImg.src = '';
   }
 }
 
--- services/getInputWeather.js
+++ services/getInputWeather.js
@@ -1,8 +1,8 @@
+const API_URL = 'https://api.openweathermap.org/data/2.5/weather?'
 const API_KEY = import.meta.env.VITE_API_KEY
-const API_URL = "https://api.openweathermap.org/data/2.5/weather?"
 
-export async function getUserWeather(lat, lon) {
-  const url = `${API_URL}lat=${lat}&lon=${lon}&appid=${API_KEY}`
+export async function getInputWeather(city) {
+  const url = `${API_URL}q=${city}&appid=${API_KEY}`
   const response = await fetch(url)
   if (!response.ok) {
     return {
